cfa66de2b4400a97e615a93fc0acb0f3b1c433fd
AlvaroRe <arb952@gmail.com>
2017-01-06 16:57:31 +0100

    DiagonalDifference Done

---
+++ HackerRank/Algorithms/Warmup/DiagonalDifference.java
@@ -0,0 +1,21 @@
+package HackerRank.Algorithms.Warmup;
+
+import java.util.Scanner;
+
+public class DiagonalDifference {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int d1 = 0, d2 = 0;
+		for(int i = 0; i < n; ++i){
+			String[]line = sc.nextLine().split(" ");
+			d1 += Integer.parseInt(line[i]);
+			d2 += Integer.parseInt(line[n-1-i]);
+		}
+		sc.close();
+		System.out.println(Math.abs(d1-d2));
+		
+	}
+
+}
